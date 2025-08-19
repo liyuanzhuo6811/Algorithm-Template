8d54dce93b1a56ae6cf1a737f729f70e93c54f13
lotus-grass <lotus_grass@outlook.com>
2025-08-19 11:08:54 +0800

    从lhc那搞来的

--- 图论Graph/shortest-path/dijkstra.cpp
+++ 图论Graph/shortest-path/dijkstra.cpp
@@ -1,38 +1,35 @@
-/*
-Dijkstra支持求单源最短路，不支持负权。
-可以通过修改一些代码来支持最长/最大最小路等。
-
-需要以下头文件：
-vector, queue, cstring
-
-最初编辑人：SnowFlavour
-最后修改人：SnowFlavour
-*/
-struct Edge {
-    int v, w;
-    Edge() {}
-    Edge(int vv, int ww) : v(vv), w(ww) {}
-    bool operator<(Edge x) const { return w < x.w; }
-    bool operator>(Edge x) const { return w > x.w; }
+struct edge
+{
+ int v, w;
 };
-const int N = 1e5 + 10;
-int dis[N];
-bool vis[N];
-vector<Edge> e[N];
-inline void Dijkstra(int s) {
-    priority_queue<Edge, vector<Edge>, greater<Edge>> q; // 通过将greater换成less可以实现最长路
-    memset(dis, 0x3f, sizeof(dis));
-    dis[s] = 0;
-    q.push(Edge(s, 0));
-    while (!q.empty()) {
-        auto [u, now] = q.top();
-        q.pop();
-        if (vis[u]) continue;
-        vis[u] = 1;
-        for (auto [v, w] : e[u]) {
-            if (now + w < dis[v]) {                      // 改成max，min等可以支持最长路径最短等
-                q.push(Edge(v, dis[v] = now + w));       // 这里同理
-            }
-        }
-    }
+struct node
+{
+ int dis, u;
+ bool operator>(const node &a) const { return dis > a.dis; }
+};
+vector<edge> e[MAXN];
+int dis[MAXN], vis[MAXN];
+priority_queue<node, vector<node>, greater<node>> q;
+void dijkstra(int n, int s)
+{
+ memset(dis, 0x3f, (n + 1) * sizeof(int));
+ memset(vis, 0, (n + 1) * sizeof(int));
+ dis[s] = 0;
+ q.push({0, s});
+ while (!q.empty())
+ {
+  int u = q.top().u;
+  q.pop();
+  if (vis[u]) continue;
+  vis[u] = 1;
+  for (auto ed : e[u])
+  {
+   int v = ed.v, w = ed.w;
+   if (dis[v] > dis[u] + w)
+   {
+    dis[v] = dis[u] + w;
+    q.push({dis[v], v});
+   }
+  }
+ }
 }
--- 图论Graph/shortest-path/spfa.cpp
+++ 图论Graph/shortest-path/spfa.cpp
@@ -1,54 +1,34 @@
-/*
-SPFA支持求单源最短路，支持负权，如果需要判断负环则需要添加 #define NEG_RING。
-可以通过修改一些代码来支持最长/最大最小路等。
-期望复杂度O(kn)，可能会退化成O(nm)。
-
-需要以下头文件：
-vector, queue, cstring
-
-最初编辑人：SnowFlavour
-最后修改人：SnowFlavour
-*/
-struct Edge {
-    int v, w;
-    Edge() {}
-    Edge(int vv, int ww) : v(vv), w(ww) {}
-    bool operator<(Edge x) const { return w < x.w; }
-    bool operator>(Edge x) const { return w > x.w; }
-};
-const int N = 1e5 + 10;
-vector<Edge> e[N];
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+int n, m;
+vector<pair<int, int>> g[N];
+inline void add(int u, int v, int w) { g[u].push_back({v, w}); }
+queue<int> q;
 int dis[N];
 bool inq[N];
-#ifdef NEG_RING
-int cnt[N], n;
-inline bool SPFA(int s) {
-#else
-inline void SPFA(int s) {
-#endif
-    queue<int> q;
-    memset(dis, 0x3f, sizeof(dis));
-    q.push(s);
-    inq[s] = 1;
-    while (!q.empty()) {
-        int u = q.front();
-        q.pop();
-        inq[u] = 0;
-#ifdef NEG_RING
-        if (cnt[u] > n) return false;
-#endif
-        for (auto [v, w] : e[u]) {
-            if (inq[v]) continue;
-            if (dis[u] + w < dis[v]) {
-                q.push(v);
-                dis[v] = dis[u] + w;
-#ifdef NEG_RING
-                cnt[v]++;
-#endif
-            }
-        }
-    }
-#ifdef NEG_RING
-    return true;
-#endif
+inline void spfa(int s)
+{
+ memset(dis, 0x3f, sizeof(dis));
+ memset(inq, false, sizeof(inq));
+ inq[s] = true, dis[s] = 0;
+ q.push(s);
+ while (q.size())
+ {
+  int u = q.front();
+  q.pop(), inq[u] = false;
+  for (auto [v, w] : g[u])
+   if (dis[v] > dis[u] + w)
+   {
+    dis[v] = dis[u] + w;
+    if (!inq[v]) q.push(v);
+   }
+ }
+}
+int main()
+{
+ cin >> n >> m;
+ for (int i = 1, u, v, w; i <= m; i++) cin >> u >> v >> w, add(u, v, w), add(v, u, w);
+ spfa(1);
+ return 0;
 }
