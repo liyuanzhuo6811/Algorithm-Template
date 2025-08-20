e9858ff60bd32c614c3a0d70bd8da0cdc158fc28
liyuanzhuo6811 <liyuanzhuo6811@outlook.com>
2025-08-20 17:05:25 +0800

    赢

--- autoformat.py
+++ autoformat.py
@@ -1,4 +1,9 @@
 import os
 print(os.getcwd())
 for dirpath, dirnames, filenames in os.walk(os.getcwd()):
-    pass
+    print("当前目录：", dirpath)
+    for file in filenames:
+        if ".cpp" in file:
+            if file[-4:] == ".cpp":
+                print(f"Formating:{dirpath}/{file}")
+                os.system(f"clang-format --style=file \"{dirpath}\\{file}\" -i")
--- 数学Math/number-theory/lucas/lucas.cpp
+++ 数学Math/number-theory/lucas/lucas.cpp
@@ -1,5 +1,4 @@
-long long Lucas(long long n, long long k long long p)
-{
- if (k == 0) return 1;
- return (C(n % p, k % p, p) * Lucas(n / p, k / p, p)) % p;
+long long Lucas(long long n, long long k, long long p) {
+    if (k == 0) return 1;
+    return (C(n % p, k % p, p) * Lucas(n / p, k / p, p)) % p;
 }
