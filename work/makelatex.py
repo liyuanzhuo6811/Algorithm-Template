0157cdec630645ed7ca362cd876c10b024299f26
liyuanzhuo6811 <liyuanzhuo6811@outlook.com>
2025-08-24 13:27:13 +0800

    坏了忘了还有暂存更改

--- work/makelatex.py
+++ work/makelatex.py
@@ -1,60 +1,87 @@
 import os
 print(os.getcwd())
-tex = open("temp1/test.tex", "w")
-tex.write("""\documentclass[12pt, a4paper]{book}
-\usepackage{ctex}
-\usepackage{authblk}
-\usepackage{listings}
-\usepackage{color}
-\usepackage{geometry}
-\usepackage{titlesec}
+titf = ["chapter", "section", "subsection", "subsubsection", "paragraph", "subparagraph"]
+tex = open("work\\temp1\\test.tex", "w", encoding="utf8")
+tex.write("""\\documentclass[9pt, a4paper, oneside]{book}
+\\usepackage{ctex}
+\\usepackage{authblk}
+\\usepackage{listings}
+\\usepackage{color}
+\\usepackage{geometry}
+\\usepackage{titlesec}
 
-\titleformat{\chapter}
-{\normalfont\Large\bfseries}{第 \thechapter 部分}{1em}{}
+\\titleformat{\\chapter}
+{\\normalfont\\Large\\bfseries}{第 \\thechapter 部分}{1em}{}
 
-\titleformat{\section}
-{\normalfont\Large\bfseries}{\thesection}{1em}{}
+\\titleformat{\\section}
+{\\normalfont\\Large\\bfseries}{\\thesection}{1em}{}
 
-\titleformat{\subsection}
-{\normalfont\large\bfseries}{\thesubsection}{1em}{}
+\\titleformat{\\subsection}
+{\\normalfont\\large\\bfseries}{\\thesubsection}{1em}{}
 
-\titleformat{\subsubsection}
-{\normalfont\normalsize\bfseries}{\thesubsubsection}{1em}{}
-\geometry{left=3.18cm, right=3.18cm, top=2.54cm, bottom=2.54cm}
-\title{\Huge XCPC Template Library}
-\author{
-    Wu Honglin / real01bit \\
-    Li Yuanzhuo / SnowFlavour \\
-    Liu Haocheng / lotus\_grass \\
+\\titleformat{\\subsubsection}
+{\\normalfont\\normalsize\\bfseries}{\\thesubsubsection}{1em}{}
+\\geometry{left=3.18cm, right=3.18cm, top=2.54cm, bottom=2.54cm}
+\\title{\\Huge XCPC Template Library}
+\\author{
+    Wu Honglin / real01bit \\\\
+    Li Yuanzhuo / SnowFlavour \\\\
+    Liu Haocheng / lotus\\_grass \\\\
 }
-\date{最后一次修改：\today}
-\newfontfamily\firacode{FiraCode-Regular.ttf}
-\lstset{
+\\date{最后一次修改：\\today}
+\\newfontfamily\\firacode{FiraCode-Regular.ttf}
+\\lstset{
     language=C++,
-    basicstyle=\firacode,
+    basicstyle=\\firacode\\small,
     breaklines=true,
-    keywordstyle=\bfseries\color{red},
-    stringstyle=\color{green}\ttfamily,
+    keywordstyle=\\bfseries\\color{red},
+    stringstyle=\\color{green}\\ttfamily,
     morekeywords={},
     emph={self},
-    emphstyle=\bfseries,
-    commentstyle=\itshape,
-    stringstyle=\bfseries,
+    emphstyle=\\bfseries,
+    commentstyle=\\itshape,
+    stringstyle=\\bfseries,
     columns=flexible,
     numbers=left,
     numbersep=2em,
-    numberstyle=\footnotesize,
+    numberstyle=\\footnotesize,
     frame=topline,
     framesep=1em
 }
-\begin{document}
-\maketitle
+\\begin{document}
+\\maketitle
+\\tableofcontents
 """)
-for dirpath, dirnames, filenames in os.walk(os.getcwd()):
-    print("当前目录：", dirpath)
-    tex.write(f"\\chapter\{{{dirpath}}}\n")
-    for file in filenames:
-        if ".cpp" in file:
-            if file[-4:] == ".cpp":
-                print(f"Working:{dirpath}/{file}")
-                
+
+# exit(0)
+def gci(filepath, rootpath):
+    files = os.listdir(filepath)
+    if ".makelatex-ignore" in files or ".git" in filepath or ".vscode" in filepath:
+        return
+    print(filepath)
+    totc = list(rootpath).count("\\")
+    nowc = list(filepath).count("\\")
+    # print(totc, nowc)
+    # print(nowc - totc)
+    # print(fi)
+    red = filepath.split("\\")[-1]
+    if nowc > totc:
+        tex.write(f"\\{titf[nowc - totc - 1]}{{{red}}}\n")
+
+    for fi in files:
+        fi_d = os.path.join(filepath, fi)
+        if os.path.isdir(fi_d):
+            gci(fi_d, rootpath)
+    for fi in files:
+        fi_d = os.path.join(filepath, fi)
+        if not os.path.isdir(fi_d):
+            if fi_d[-4:] == ".cpp":
+                tex.write(f"\\{titf[nowc - totc]}{{{fi.replace("_", "\\_")}}}\n")
+                with open(fi_d, encoding="utf8") as tmp:
+                    code = tmp.read()
+                    tex.write("\\begin{lstlisting}[language={C++}]\n")
+                    tex.write(code)
+                    tex.write("\\end{lstlisting}\n")
+
+gci(os.getcwd(), os.getcwd())
+tex.write("\\end{document}")
