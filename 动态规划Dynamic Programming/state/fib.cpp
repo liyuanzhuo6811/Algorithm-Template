1b0b7ff3f9213a9f6a977d320a0ce300d740342a
lotus-grass <lotus_grass@outlook.com>
2025-11-12 08:54:16 +0800

    update state

---
+++ 动态规划Dynamic Programming/state/fib.cpp
@@ -0,0 +1,49 @@
+// {"name":"ddp","intro":“$O(n (fib(m))^2)$”}
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 26, mod = 1e9 + 7;
+int n, m, E[N];
+char s[N][N];
+size_t dp[2][(1 << 24) + 5];
+vector<int> g;
+int main()
+{
+ ios_base::sync_with_stdio(false);
+ cin.tie(nullptr);
+ cout.tie(nullptr);
+ cin >> n >> m;
+ for (int i = 1; i <= n; ++i)
+ {
+  cin >> s[i];
+  for (int j = 0; j < m; ++j)
+   if (s[i][j] == '#') E[i] |= (1 << j);
+ }
+ for (int S = 0; S < (1 << m); ++S)
+  if (!(S & (S << 1))) g.push_back(S);
+ auto check = [&](int S, int T, int x) -> bool
+ {
+  if (S & T) return false;
+  if (T & (T << 1)) return false;
+  if ((S << 1) & T) return false;
+  if ((S >> 1) & T) return false;
+  if (T & E[x]) return false;
+  return true;
+ };
+ int cur = 0, pre = 1;
+ dp[0][0] = 1;
+ for (int i = 1; i <= n; ++i)
+ {
+  swap(pre, cur);
+  for (int S : g) dp[cur][S] = 0;
+  for (int S : g)
+  {
+   if (!dp[pre][S]) continue;
+   for (int T : g)
+    if (check(S, T, i)) (dp[cur][T] += dp[pre][S]) %= mod;
+   }
+ }
+ size_t ans = 0;
+ for (int S = 0; S < (1 << m); ++S) (ans += dp[cur][S]) %= mod;
+ cout << ans << endl;
+ return 0;
+}
